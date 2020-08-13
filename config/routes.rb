0b236285e89190e31265c44bb177af833e61fc69
Noor Makria noor.makria@gmail.com <noor.makria@gmail.com>
2020-08-13 15:22:16 +0200

    Etape 4 - Routing, Controller, Views for Doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :cocktails, only: [:show, :index, :new, :create] do
+  resources :doses, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :doses, only: [:destroy]
 end
